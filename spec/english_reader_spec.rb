d3c0b850af47cb44be553db3decfe475aff5b3f8
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-04 17:34:52 -0700

    Test: Write test to translate multiple letters at once

--- spec/english_reader_spec.rb
+++ spec/english_reader_spec.rb
@@ -9,6 +9,9 @@ describe EnglishReader do
     @file2 = './spec/braille_test.txt'
     @file3 = './spec/message_test_2.txt'
     @file4 = './spec/braille_test_2.txt'
+    @file5 = './spec/message_test_3'
+    @file6 = './spec/braille_test_3.txt'
+
     @e_reader = EnglishReader.new(@file1, @file2)
   end
 
@@ -84,14 +87,23 @@ describe EnglishReader do
   context 'writing in braille' do
     before :each do
       @e_reader_2 = EnglishReader.new(@file3, @file4)
+      @e_reader_3 = EnglishReader.new(@file5, @file5)
     end
 
-    it 'EnglishReader #write_braille can write braille to a given file' do
+    it '#write_braille can translate a single letter to a given file' do
       first_expected = ["O.\n", "..\n", '..']
       @e_reader_2.write_braille
       first_actual = File.readlines(@file4)[0..3]
 
       expect(first_actual).to eq(first_expected)
+    end
+
+    it '#write_braille can write multiple letters to a file' do
+      second_expected = ["O.O.O.O.O.\n", "..........\n", ".........."]
+      @e_reader_3.write_braille
+      second_actual = File.readlines(@file6)[0..3]
+
+      expect(second_actual).to eq(second_expected)
     end
   end
 
