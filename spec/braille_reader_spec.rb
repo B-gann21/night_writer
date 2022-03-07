79031a345bd82d5a21d8dfd4403bb2f1511efb3c
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-07 10:46:26 -0700

    test: Write test for #translate_braille

--- spec/braille_reader_spec.rb
+++ spec/braille_reader_spec.rb
@@ -2,6 +2,8 @@ require './lib/braille_reader'
 
 describe BrailleReader do
   before :each do
+    @braille_a = ['O.', '..', '..']
+    @braille_b = ['O.', 'O.', '..']
     @file1 = './spec/message_test_2.txt'
     @file2 = './spec/braille_test_2.txt'
     @b_reader = BrailleReader.new(@file2, @file1)
@@ -15,6 +17,20 @@ describe BrailleReader do
     it 'can print an info message' do
       expected = 'Created message_test_2.txt containing 1 characters'
       expect(@b_reader.info_msg).to eq(expected)
+    end
+  end
+
+  context 'translation' do
+    it '#translate_braille converts braille chars to english' do
+      actual = @b_reader.translate_braille(@braille_a)
+
+      expect(actual).to eq("a")
+    end
+
+    it 'can translate different characters' do
+      actual = @b_reader.translate_braille(@braille_b)
+
+      expect(actual).to eq(expected)
     end
   end
 end
