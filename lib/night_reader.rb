a41854936dff896f5f90cf80cecb386cfd4defc8
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-08 13:51:38 -0700

    Refactor: Runner files had the wrong #write methods

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -2,5 +2,5 @@ require_relative 'braille_reader'
 
 b_reader = BrailleReader.new(ARGV[0], ARGV[1])
 
-b_reader.write_braille
+b_reader.write_english
 puts b_reader.info_msg
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,5 +2,5 @@ require_relative 'english_reader'
 
 e_reader = EnglishReader.new(ARGV[0], ARGV[1])
 
-e_reader.write_english
+e_reader.write_braille
 puts e_reader.info_msg
