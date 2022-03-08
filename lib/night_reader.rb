ae7ad9dc206a6f7446bb3bdd83c32041f751a480
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-08 13:28:24 -0700

    Refactor: add #write methods to respective runner files

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -2,4 +2,5 @@ require_relative 'braille_reader'
 
 b_reader = BrailleReader.new(ARGV[0], ARGV[1])
 
+b_reader.write_braille
 puts b_reader.info_msg
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,4 +1,6 @@
 require_relative 'english_reader'
 
 e_reader = EnglishReader.new(ARGV[0], ARGV[1])
+
+e_reader.write_english
 puts e_reader.info_msg
